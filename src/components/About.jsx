8b6893f963a45046aec7dc312035b00399ff9fac
unknown <abuthahirchalissery162@gmail.com>
2025-12-24 21:41:43 +0530

    class component added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,13 @@
+import User from "./User"
+import UserClass from "./UserClass"
 
 const About = () => {
     return(
         <div>
             <h1>This is about page</h1>
+            <User name={"Abuthahir ( Function component )"}/>
+
+            <UserClass name={"Abuthahir from class component"}/>
         </div>
     )
 }
---
+++ src/components/User.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const User = (props) => {
+    const [count]  = useState(0)
+    const { name } = props;
+    return(
+        <div className="user-card">
+            <h1>Name: {name}</h1>
+            <h1>Location: Chalissery</h1>
+            <h1>Contact: 9778238561</h1>
+            <h1>count : {count}</h1>
+        </div>
+    )
+}
+
+export default User;
